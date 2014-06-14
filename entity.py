32292cbfd5ade4d4b3ffa29610d5266760f30bcd
Cannon Lewis <cannon10100@gmail.com>
2014-06-14 09:44:34 -0500

    Fixed text showing, made things much prettier

--- entity.py
+++ entity.py
@@ -5,6 +5,8 @@ class Entity():
     """Class used to represent different data objects, differences defined by type string. (Subclasses?)"""
 
     def __init__(self, surf, center, type="circle", radius=80):
+        pygame.font.init()
+        self.font = pygame.font.Font(None, 30)
         self.surf = surf
         self.center = center
         self.pos = [center[0] - radius, center[1] - radius]
@@ -14,6 +16,7 @@ class Entity():
         self.acc = [0.0, 0.0]
         self.z_val = 0
         self.text = ""
+        self.transformed_surf = None
         self.title = ""
         self.connections = []
 
@@ -28,4 +31,10 @@ class Entity():
 
     def render(self, target_surf):
         assert isinstance(target_surf, pygame.Surface), "target_surf must be a Surface"
-        target_surf.blit(pygame.transform.scale(self.surf, (self.radius * 2, self.radius * 2)), self.pos)
+        self.transformed_surf = pygame.transform.scale(self.surf, (self.radius * 2, self.radius * 2))
+        target_surf.blit(self.transformed_surf, self.pos)
+        text_render = self.font.render(self.title, 1, (0, 0, 0))
+        text_rect = text_render.get_rect()
+        text_rect.centerx = self.center[0]
+        text_rect.centery = self.center[1]
+        target_surf.blit(text_render, text_rect)
